00e5e78ca4abcfe088f2c67f646e373f0a00c9ce
AlaaRashed <114382094+alaaRashed2001@users.noreply.github.com>
2026-07-24 11:39:27 +0300

    -Sent Message Card -Response Message Card -Put Messages In FlatList -Chat Input -KeyboardAvoidingView

---
+++ src/components/ResponseMessageCard.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React, { FC } from 'react';
+import { s, vs } from 'react-native-size-matters';
+import { colors } from '../styles/colors';
+
+interface ResponseMessageCardProps {
+  message: string;
+}
+
+const ResponseMessageCard: FC<ResponseMessageCardProps> = ({ message }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.messageContainer}>
+        <Text style={styles.messageText}>{message}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ResponseMessageCard;
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: vs(4),
+    marginBottom: vs(12),
+  },
+  messageContainer: {
+    backgroundColor: colors.grayBack,
+    borderRadius: s(20),
+    maxWidth: '80%',
+    padding: s(10),
+  },
+  messageText: {
+    color: colors.black,
+    fontSize: s(16),
+  },
+});
---
+++ src/screens/ChatScreen.tsx
@@ -0,0 +1,95 @@
+import { FlatList, StyleSheet, KeyboardAvoidingView, View } from 'react-native';
+import React, { useState } from 'react';
+import AppHeader from '../components/AppHeader';
+import SentMessageCard from '../components/SentMessageCard';
+import ResponseMessageCard from '../components/ResponseMessageCard';
+import { s } from 'react-native-size-matters';
+import { RECEIVED, SENT } from '../constants/chat';
+import ChatInput from '../components/ChatInput';
+
+interface IChatScreen {
+  id: number;
+  message: string;
+  type: string;
+}
+const ChatScreen = () => {
+  const messagesList: IChatScreen[] = [
+    {
+      message: 'Hello',
+      type: SENT,
+      id: 1,
+    },
+    {
+      message: 'Hello',
+      type: RECEIVED,
+      id: 2,
+    },
+  ];
+  const [messagesData, setMessagesData] = useState<IChatScreen[]>(messagesList);
+  const [msgInput, setMsgInput] = useState('');
+
+  // Function to send a new Message to AI
+  const onMessageSent = () => {
+    console.log('user type', msgInput);
+
+    setMessagesData(prevMessages => {
+      return [
+        ...prevMessages,
+        {
+          message: msgInput,
+          id: prevMessages.length + 1,
+          type: SENT,
+        },
+      ];
+    });
+
+    setTimeout(() => {
+      onGetResponse('Hello, I am AI Assistant, How can I help you today?');
+    }, 2000);
+  };
+
+  // Function to receive statice response
+  const onGetResponse = (response: string) => {
+    setMessagesData(prevMessages => {
+      return [
+        ...prevMessages,
+        {
+          message: response,
+          id: prevMessages.length + 1,
+          type: RECEIVED,
+        },
+      ];
+    });
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+        <AppHeader />
+
+        <FlatList
+          data={messagesData}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => {
+            return item.type === SENT ? (
+              <SentMessageCard message={item.message} />
+            ) : (
+              <ResponseMessageCard message={item.message} />
+            );
+          }}
+          contentContainerStyle={{ paddingHorizontal: s(8) }}
+        />
+
+        <ChatInput
+          messageValue={msgInput}
+          setMessageValue={setMsgInput}
+          onMessageSent={onMessageSent}
+        />
+      </KeyboardAvoidingView>
+    </View>
+  );
+};
+
+export default ChatScreen;
+
+const styles = StyleSheet.create({});
